ca5e25e86f86e67b9fdbaf9fb3528b631eda9b7b
chill-czar <aisarthak2@gmail.com>
2025-07-14 17:36:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.html", // include all HTML files in the root
+    "./**/*.html", // include HTML files in subdirectories
+  ],
+  plugins: [],
+};
